d73c116837272a537521fa4d83963f7a575e87a6
Derek Deng <derek.deng@hirede.com>
2016-01-10 22:35:45 +0800

    实现dtip功能

--- dtip.js
+++ dtip.js
@@ -4,5 +4,57 @@
  * https://github.com/dengzz/dtip.js
  *
  * Example:
-    TODO
+    dtip("hi world!");
  */
+; (function (factory) {
+    factory(window.jQuery);
+} (function ($) {
+    "use strict";
+    var timeout = 0, container = null;
+    var remove  = function () {
+        document.body.removeChild(container);
+    };
+    var clear = function () {
+        if (timeout > 0) {
+            clearTimeout(timeout);
+            timeout = 0;
+            remove();
+        }
+    };
+    var fadeOut = function () {
+        $(container).fadeOut("slow", function () {
+            clear();
+        });
+    };
+    var create = function (text) {
+        container = document.createElement("div");
+        $.extend(container.style, {
+            position: "fixed",
+            width: "100%",
+            top: "30%",
+            textAlign: "center",
+            zIndex: "9999"
+        });
+        var textTag = document.createElement("span");
+        $.extend(textTag.style, {
+            background: "#000",
+            opacity: "0.6",
+            filter: "alpha(opacity=60)",
+            zIndex: "-1",
+            border: "1px solid #666",
+            padding: "10px 20px",
+            color: "#fff",
+            display: "inline-block",
+            fontSize: "12px",
+            borderRadius: "5px"
+        });
+        container.appendChild(textTag);
+        textTag.innerText = text;
+        document.body.appendChild(container);
+    };
+    window.dtip = function (text) {
+        clear();
+        timeout = setTimeout(fadeOut, 1500);
+        create(text);
+    };
+}));
